a844d0cf039635740912eb30ea79c9495144acea
kw2019ltd <kw.2019.ltd@gmail.com>
2021-06-15 17:28:42 +0300

    version: 3.0.1

--- lib/utils.dart
+++ lib/utils.dart
@@ -13,49 +13,59 @@ Uint8List _concatenatePlanes(List<Plane> planes) {
   return allBytes.done().buffer.asUint8List();
 }
 
-FirebaseVisionImageMetadata buildMetaData(
-  CameraImage image,
-  ImageRotation rotation,
-) {
-  return FirebaseVisionImageMetadata(
-    rawFormat: image.format.raw,
-    size: Size(image.width.toDouble(), image.height.toDouble()),
-    rotation: rotation,
-    planeData: image.planes
-        .map(
-          (plane) => FirebaseVisionImagePlaneMetadata(
-            bytesPerRow: plane.bytesPerRow,
-            height: plane.height,
-            width: plane.width,
-          ),
-        )
-        .toList(),
-  );
-}
-
 Future<T> _detect<T>(
   CameraImage image,
   HandleDetection<T> handleDetection,
-  ImageRotation rotation,
+  InputImageRotation rotation,
 ) async {
-  return handleDetection(
-    FirebaseVisionImage.fromBytes(
-      _concatenatePlanes(image.planes),
-      buildMetaData(image, rotation),
-    ),
-  );
+  return handleDetection(_processCameraImage(image));
 }
 
-ImageRotation _rotationIntToImageRotation(int rotation) {
+InputImage _processCameraImage(CameraImage image) {
+  final WriteBuffer allBytes = WriteBuffer();
+  for (Plane plane in image.planes) {
+    allBytes.putUint8List(plane.bytes);
+  }
+  final bytes = allBytes.done().buffer.asUint8List();
+
+  final Size imageSize = Size(image.width.toDouble(), image.height.toDouble());
+
+  final imageRotation = InputImageRotation.Rotation_0deg;
+
+  final inputImageFormat =
+      InputImageFormatMethods.fromRawValue(image.format.raw) ??
+          InputImageFormat.NV21;
+
+  final planeData = image.planes.map(
+    (Plane plane) {
+      return InputImagePlaneMetadata(
+        bytesPerRow: plane.bytesPerRow,
+        height: plane.height,
+        width: plane.width,
+      );
+    },
+  ).toList();
+
+  final inputImageData = InputImageData(
+    size: imageSize,
+    imageRotation: imageRotation,
+    inputImageFormat: inputImageFormat,
+    planeData: planeData,
+  );
+
+  return InputImage.fromBytes(bytes: bytes, inputImageData: inputImageData);
+}
+
+InputImageRotation _rotationIntToImageRotation(int rotation) {
   switch (rotation) {
     case 0:
-      return ImageRotation.rotation0;
+      return InputImageRotation.Rotation_0deg;
     case 90:
-      return ImageRotation.rotation90;
+      return InputImageRotation.Rotation_90deg;
     case 180:
-      return ImageRotation.rotation180;
+      return InputImageRotation.Rotation_180deg;
     default:
       assert(rotation == 270);
-      return ImageRotation.rotation270;
+      return InputImageRotation.Rotation_270deg;
   }
 }
